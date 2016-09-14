a7e5e8cf386951cdff215a594499c9f06fac51b5
Olivier Rousseau <zeachco@gmail.com>
2016-09-15 01:52:51 +0530

    prompt login on 403 errors

--- src/store/index.js
+++ src/store/index.js
@@ -8,4 +8,15 @@ import {createStore} from 'redux';
 
 const store = createStore(reducers);
 
+import axios from 'axios';
+axios.interceptors.response.use(function(response) {
+  return response;
+}, function(err) {
+  console.log(err);
+  if (err.response.status === 403) {
+    store.dispatch({type: 'SESSION_LOGIN_FAIL'});
+  }
+  return Promise.reject(err);
+});
+
 export default store;
