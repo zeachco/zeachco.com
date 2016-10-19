3c3864affc282d28d21143c33546614753f172e1
Olivier Rousseau-Guyot <zeachco@gmail.com>
2016-10-19 17:00:04 -0400

    clean unused

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,5 @@
 import reducers from './reducers';
-// import {applyMiddleware, createStore} from 'redux';
-// import logger from 'redux-logger';
 import {createStore} from 'redux';
-
-// const middlewares = applyMiddleware(logger('dev'));
-// const store = createStore(reducers, middlewares);
 
 const store = createStore(reducers);
 
