9b9f66f87ccb0956b8192bb6e672935821ac8456
Olivier Rousseau-Guyot <zeachco@gmail.com>
2016-11-23 21:20:35 -0500

    fetching categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import {Base} from '.';
+import React, {
+  Component
+} from 'react';
+import {
+  Base
+} from '.';
+import axios from 'axios';
 
-export const Categories = props => (
-  <Base>
-    <h2>Catégories</h2>
-    Cette page est en construction...
-  </Base>
-);
+export class Categories extends Component {
+  constructor(...args) {
+    super(...args)
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentWillMount(newprops) {
+    this.setState({
+      loading: true
+    });
+    axios
+      .get('/api/admin/categories')
+      .then(xhr => {
+        this.setState({
+          categories: xhr.data,
+          loading: false
+        });
+      }).catch(error => {
+        this.setState({
+          loading: false
+        });
+      });
+  }
+
+  render() {
+    const {categories} = this.state;
+    return (
+      <Base>
+        <h2>Catégories ({categories.length})</h2>
+        <p>Cette liste est en lecture seule pour l'instant</p>
+        {categories.map(({label, value}) => (<div key={value}>{label}</div>))}
+      </Base>
+    );
+  }
+}
