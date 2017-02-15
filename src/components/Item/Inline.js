d059fb61000ede083ad09a674236f49b5c870bb7
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-02-15 17:46:05 -0500

    added labels and space information

--- src/components/Item/Inline.js
+++ src/components/Item/Inline.js
@@ -17,8 +17,12 @@ export const ItemInline = ({
         backgroundImage: `url(${files[0]}/thumb)`,
         height: '1.5em',
         width: '3em'
-      }}></span>&nbsp;<span>{name}</span>
-    </h4>
-    <p>{labels && labels.join(', ')}</p>
+        }}></span>&nbsp;<span>{name}</span>
+        <br />
+        <small>
+          <span className="label label-primary">{space}</span>
+          {labels.map(label => (<span> <span className="label label-info">{label}</span></span>))}
+        </small>
+      </h4>
   </div>
 );
