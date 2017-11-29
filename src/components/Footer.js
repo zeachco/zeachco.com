7eeb8206b9e6c681171af30d1a1525f5476ebbba
olivierr <olivierr@playtika.com>
2017-11-29 16:05:30 -0500

    added linkedin reference

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinkedInIcon from 'react-icons/lib/fa/linkedin-square'
 
 import LangSwitcher from './LangSwitcher'
 import Translate from './Translate'
@@ -8,7 +9,10 @@ const Footer = () => (
     <div className="container">
       <LangSwitcher />
       <hr/>
-      <p className="text-muted"><Translate content="Inquiries" data={{email: 'zeachco@gmail.com'}} /></p>
+      <p className="text-muted">
+        <Translate content="Inquiries" data={{email: 'zeachco@gmail.com'}} />
+        <a className="pull-right" href="//www.linkedin.com/in/zeachco"><LinkedInIcon />{' '}linkedin.com/in/zeachco</a>
+      </p>
     </div>
   </footer>
 );
