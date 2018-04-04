1a7a8b1f3c57eab3eec8a205feb2ef3f0aec6f1f
Olivier Rousseau-Guyot <zeachco@gmail.com>
2018-04-04 11:01:45 -0400

    cors page style

--- src/pages/Cors.js
+++ src/pages/Cors.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+import './Cors.css';
 import Base from './Base';
 
 class CorsPage extends Component {
@@ -7,10 +8,18 @@ class CorsPage extends Component {
     // safari default settings force the auth domain to have been visited and a cookie to be set
     document.cookie = "username=John Doe";
   }
+
+  close() {
+    window.close();
+  }
+
   render() {
     return (
       <Base>
-        <h2>this domain have been authorized</h2>
+        <div className="cors-message">
+          <h2>this domain have been authorized</h2>
+          <button className="btn btn-primary" onClick={this.close.bind(this)}>Close this window</button>
+        </div>
       </Base>
     );
   }
